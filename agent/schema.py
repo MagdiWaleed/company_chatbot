9dd3f1fb20fd6a17b971c6d02abc365e6c2c6ab4
MagdiWaleed <94909647+MagdiWaleed@users.noreply.github.com>
2025-08-23 20:46:41 +0300

    split the software to frontend and backend

--- agent/schema.py
+++
@@ -1,7 +0,0 @@
-from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
-from langgraph.graph import StateGraph, START,END
-from langgraph.graph.message import add_messages
-from typing import TypedDict, Annotated, List
-
-class AgentState(TypedDict):
-    messages: Annotated[List[BaseMessage],add_messages]
